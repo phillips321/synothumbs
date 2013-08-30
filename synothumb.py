f9d4a3446e46a6ce72c5605ffa5462348814eaec
phillips321 <phillips321@3ea00826-d0a1-99c7-a044-13a2c7e5b285>
2013-08-30 16:55:10 +0000

    synothumb.py v1.0

--- synothumb.py
+++ synothumb.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 # sudo mount_nfs -P 192.168.0.2:/volume1/photo /Users/phillips321/nfsmount
+# Author:       phillips321
+# License:      CC BY-SA 3.0
+# Use:          home use only, commercial use by permission only
+# Released:     www.phillips321.co.uk
+# Dependencies: PIL, libjpeg, libpng
+# Supports:     jpg, bmp, png, tif
+# Version:      1.0
+# ChangeLog:
+#       v1.0 - First release
+# ToDo:
+#       add movie support
 import os,sys,Image
+#import Queue,threading
+#queue = Queue.Queue
+extensions=['.jpg','.png','.jpeg','.tif','.bmp']
+skipfiles=['.DS_Store','.apdisk','Thumbs.db']
 try:
     rootdir=sys.argv[1]
 except:
@@ -8,12 +23,15 @@ except:
     sys.exit(0)
 imageList=[]
 
-# Finds all images of type jpg,png,jpeg,tif,bmp
+# Finds all images of type in extensions array
 for path, subFolders, files in os.walk(rootdir):
     for file in files:
-        if os.path.splitext(file)[1].lower() == ".jpg" or ".png" or ".jpeg" or ".tif" or ".bmp":
+        ext=os.path.splitext(file)[1].lower()
+        if any(x in ext for x in extensions):#check if extensions matches ext
             if "@eaDir" not in path:
-                if file != ".DS_Store": imageList.append(os.path.join(path,file))
+                if file != ".DS_Store" and file != ".apdisk" and file != "Thumbs.db":
+                    print "Now searching thumbs for %s " % (os.path.join(path,file))
+                    imageList.append(os.path.join(path,file))
 
 xlName="SYNOPHOTO:THUMB_XL.jpg" ; xlSize=(1280,1280) #XtraLarge
 lName="SYNOPHOTO:THUMB_L.jpg" ; lSize=(800,800) #Large
@@ -21,26 +39,25 @@ bName="SYNOPHOTO:THUMB_B.jpg" ; bSize=(640,640) #Big
 mName="SYNOPHOTO:THUMB_M.jpg" ; mSize=(320,320) #Medium
 sName="SYNOPHOTO:THUMB_S.jpg" ; sSize=(160,160) #Small
 
+def convertImage(imagePath):
+    image=Image.open(imagePath)
+    if os.path.isfile(os.path.join(thumbDir,xlName)) != 1:
+        print "Now working on %s" % (imagePath)
+        image.thumbnail(xlSize, Image.ANTIALIAS)
+        image.save(os.path.join(thumbDir,xlName))
+        image.thumbnail(lSize, Image.ANTIALIAS)
+        image.save(os.path.join(thumbDir,lName))
+        image.thumbnail(bSize, Image.ANTIALIAS)
+        image.save(os.path.join(thumbDir,bName))
+        image.thumbnail(mSize, Image.ANTIALIAS)
+        image.save(os.path.join(thumbDir,mName))
+        image.thumbnail(sSize, Image.ANTIALIAS)
+        image.save(os.path.join(thumbDir,sName))
+
 for imagePath in imageList:
     imageDir,imageName = os.path.split(imagePath)
     thumbDir=os.path.join(imageDir,"@eaDir",imageName)
     if os.path.isdir(thumbDir) != 1:
         try:os.makedirs(thumbDir)
         except:continue
-    image=Image.open(imagePath)
-    if os.path.isfile(os.path.join(thumbDir,xlName)) != 1:
-        print "Now working on %s : %s" % (imageName,xlName)
-        image.thumbnail(xlSize, Image.ANTIALIAS)
-        image.save(os.path.join(thumbDir,xlName))
-        print "Now working on %s : %s" % (imageName,lName)
-        image.thumbnail(lSize, Image.ANTIALIAS)
-        image.save(os.path.join(thumbDir,lName))
-        print "Now working on %s : %s" % (imageName,bName)
-        image.thumbnail(bSize, Image.ANTIALIAS)
-        image.save(os.path.join(thumbDir,bName))
-        print "Now working on %s : %s" % (imageName,mName)
-        image.thumbnail(mSize, Image.ANTIALIAS)
-        image.save(os.path.join(thumbDir,mName))
-        print "Now working on %s : %s" % (imageName,sName)
-        image.thumbnail(sSize, Image.ANTIALIAS)
-        image.save(os.path.join(thumbDir,sName))
+    convertImage(imagePath)
